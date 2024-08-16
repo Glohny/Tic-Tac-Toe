a9971e3099cfd51b73767e3f004cac88d5a15c46
Glohny <rohanyusaf@gmail.com>
2024-08-16 01:53:08 -0400

    Refined playability and added minor sound effects, added scoreboard too

--- javascript.js
+++ javascript.js
@@ -1,17 +1,16 @@
-const player1 = createPlayer("Glohny","X");
-const player2 = createPlayer("Andrew","O");
+const player1 = createPlayer("Glohny","X",document.querySelector(".Score1"));
+const player2 = createPlayer("Andrew","O",document.querySelector(".Score2"));
+let AllButtons = document.querySelectorAll(".BoardButton");
 
 const GameController = (function() {
     let PlayerTurn = true
     const GetTurnSymbol = function() {
         if (PlayerTurn) {
             PlayerTurn = false;
-            console.log("Returning: " + player1)
             return player1.GetSymbol();
         }
         else {
             PlayerTurn = true;
-            console.log("Returning: " + player2)
             return player2.GetSymbol();
         }
     }
@@ -27,7 +26,11 @@ const GameController = (function() {
             (gameboard[0][0] === 'X' && gameboard[1][1] === 'X' && gameboard[2][2] === 'X') || // Main diagonal
             (gameboard[0][2] === 'X' && gameboard[1][1] === 'X' && gameboard[2][0] === 'X')    // Anti-diagonal
         ) {
-            console.log("X wins!");
+            AttributeChanger.HighlightButton();
+            AttributeChanger.ChangeScore(player1);
+            AllButtons.forEach(button => {
+                button.disabled = true;
+            });
         }
         
         // Check if O wins
@@ -41,32 +44,68 @@ const GameController = (function() {
             (gameboard[0][0] === 'O' && gameboard[1][1] === 'O' && gameboard[2][2] === 'O') || // Main diagonal
             (gameboard[0][2] === 'O' && gameboard[1][1] === 'O' && gameboard[2][0] === 'O')    // Anti-diagonal
         ) {
-            console.log("O wins!");
-        } else {
-            console.log("No winner yet!");
-        }
+            AttributeChanger.HighlightButton();
+            AttributeChanger.ChangeScore(player2);
+            AllButtons.forEach(button => {
+                button.disabled = true;
+            });
+        } 
     }
 
     const reset = function(gameboard) {
+        AllButtons.disabled = false;
         for(let i = 0; i < 3; i++) {
             for (let j = 0; j < 3; j++) {
                 gameboard[i][j] = "";
             }
         }
 
-        let AllButtons = document.querySelectorAll(".BoardButton");
-        console.log(AllButtons);
         for (let i = 0; i < 9; i++) {
             AllButtons[i].innerHTML = "";
         }
 
-        console.table(gameboard)
+        AllButtons.forEach(button => {
+            button.disabled = false;
+        });
+
+        AttributeChanger.unHighlightButton();
     }
 
     return {
         GetTurnSymbol, CheckWinner, reset
     }
 })();
+
+const AttributeChanger = (function() {
+    const ResetButton = document.querySelector(".ResetButton");
+    const HighlightButton = function() {
+        ResetButton.style.transition = ".5s";
+        ResetButton.style.height = "60px";
+        ResetButton.style.width = "210px";
+        ResetButton.style.fontSize = "50px";
+        ResetButton.style.boxShadow = "0 5px 8px -1px rgba(255, 255, 255, 0.4)";
+    }
+    
+    const unHighlightButton = function() {
+        ResetButton.style.transition = ".25s";
+        ResetButton.style.height = "50px";
+        ResetButton.style.width = "200px";
+        ResetButton.style.fontSize = "45px";
+        ResetButton.style.boxShadow = "none";
+    }
+    
+    const ChangeScore = function(player) {
+        let ScoreText = player.GetDivStation().innerHTML.substring(0,9);
+        let CurrentScore = parseInt(player.GetDivStation().innerHTML.substring(9));
+        let NewScoreText = ScoreText + (CurrentScore + 1);
+        player.GetDivStation().innerHTML = NewScoreText;
+    }
+
+    return {
+        HighlightButton, unHighlightButton, ChangeScore
+    }
+})();
+
 
 const GameBoard = (function() {
     const gameboard = [
@@ -76,8 +115,10 @@ const GameBoard = (function() {
     ];
     document.querySelector(".ResetButton").addEventListener("click", () => GameController.reset(gameboard));
     const DefineSymbol = function(pos1,pos2,but) {
+        let mySound = new Audio("Assets/SoundEffects/click.wav");
+        mySound.volume = 0.3;
+        mySound.play();
         if (gameboard[pos1][pos2] != "") {
-            console.log("Choose Different Spot");
         }
         else {
             gameboard[pos1][pos2] = GameController.GetTurnSymbol();
@@ -91,9 +132,10 @@ const GameBoard = (function() {
     };
 })();
 
-function createPlayer (name,symbol) {
+function createPlayer (name,symbol,DivStation) {
     this.name = name;
     this.symbol = symbol;
+    this.DivStation = DivStation;
     const GetName = function() {
         return symbol;
     };
@@ -101,7 +143,11 @@ function createPlayer (name,symbol) {
         return symbol;
     };
 
+    const GetDivStation = function() {
+        return DivStation;
+    };
+
     return {
-        GetName, GetSymbol
+        GetName, GetSymbol, GetDivStation
     }
 }
